821de4195f95c6b85dc017c4f73abcd2445aa608
NibidaGhimire <nibidaghimire39@gmail.com>
2023-12-27 19:13:55 +0545

    buses

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,75 +1,54 @@
-// ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk
+// // ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk
 
 
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+// import React, { useEffect } from 'react';
 
-// const Track = () => {
-//   const [map, setMap] = useState(null);
-//   const [currentLocation, setCurrentLocation] = useState(null);
-
+// const MapComponent = () => {
 //   useEffect(() => {
-//     const bingMapScript = document.createElement('script');
-//     bingMapScript.src = 'https://www.bing.com/api/maps/mapcontrol?key=ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk&callback=initMap';
-//     window.document.body.appendChild(bingMapScript);
-//     window.initMap = () => {
-//       const newMap = new Microsoft.Maps.Map(document.getElementById('map'), {
-//         center: new Microsoft.Maps.Location(0, 0),
-//         zoom: 32,
-//       });
-//       setMap(newMap);
+//     const loadMap = () => {
+//       const script = document.createElement('script');
+//       script.type = 'text/javascript';
+//       script.async = true;
+//       script.defer = true;
+//       script.src = `https://www.bing.com/api/maps/mapcontrol?key=ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk&callback=initMap`;
+//       document.body.appendChild(script);
 //     };
+
+
+//     window.initMap = () => {
+//       const map = new window.Microsoft.Maps.Map(document.getElementById('bingMap'), {
+//         credentials: "ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk",
+//       });
+
+//       if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//       } else {
+//         alert('Geolocation is not supported by this browser.');
+//       }
+
+//       function showPosition(position) {
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+
+//         const location = new window.Microsoft.Maps.Location(latitude, longitude);
+//         map.setView({ center: location, zoom: 20 });
+
+//         const pin = new window.Microsoft.Maps.Pushpin(location,{
+//           title: "bus",
+//         });
+//         map.entities.push(pin);
+//       }
+//     };
+
+//     loadMap();
 //   }, []);
 
-//   useEffect(() => {
-//     const fetchFakeBusLocation = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:3000/fake-bus-location');
-//         if (response && response.data) {
-//           const { latitude, longitude } = response.data;
-//           const newLocation = new Microsoft.Maps.Location(parseFloat(latitude), parseFloat(longitude));
-
-//           if (!currentLocation || Microsoft.Maps.SpatialMath.getDistanceTo(currentLocation, newLocation) < 1000) {
-//             updateMap(newLocation);
-//             setCurrentLocation(newLocation);
-//           }
-//         } else {
-//           console.error('Invalid response from server:', response);
-//         }
-//       } catch (error) {
-//         console.error('Error fetching fake bus location:', error.message);
-//       }
-//     };
-
-//     const updateMap = (location) => {
-//       if (!map) return;
-
-//       const pin = new Microsoft.Maps.Pushpin(location, {
-//         title: 'Bus Location',
-//       });
-
-//       if (map.entities.getLength() > 0) {
-//         map.entities.clear();
-//       }
-
-//       map.entities.push(pin);
-//       map.setView({ center: location, zoom: 12 });
-//     };
-
-//     const interval = setInterval(fetchFakeBusLocation, 5000);
-
-//     return () => clearInterval(interval);
-//   }, [map, currentLocation]);
-
-//   return (
-//     <div id='map' className='w-full h-screen' />
-//   );
+//   return(
+//      <div id="bingMap" className='w-full h-screen'></div>
+//   )
 // };
 
-// export default Track;
-
-
-
+// export default MapComponent;
 
 
 
@@ -86,10 +65,25 @@ const MapComponent = () => {
       document.body.appendChild(script);
     };
 
-
     window.initMap = () => {
       const map = new window.Microsoft.Maps.Map(document.getElementById('bingMap'), {
-        credentials: "ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk",
+        credentials: 'ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk',
+      });
+
+      const locations = [
+        { name: 'Sajha 1', latitude: 27.6796000, longitude: 85.3204300 },
+        { name: 'Nepal Yatayat 1', latitude: 27.686382, longitude: 85.315399 },
+      ];
+
+      locations.forEach((location) => {
+        const { name, latitude, longitude } = location;
+
+        const loc = new window.Microsoft.Maps.Location(latitude, longitude);
+        const pin = new window.Microsoft.Maps.Pushpin(loc, {
+          title: name,
+        });
+
+        map.entities.push(pin);
       });
 
       if (navigator.geolocation) {
@@ -102,22 +96,22 @@ const MapComponent = () => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
-        const location = new window.Microsoft.Maps.Location(latitude, longitude);
-        map.setView({ center: location, zoom: 20 });
+        const userLocation = new window.Microsoft.Maps.Location(latitude, longitude);
+        map.setView({ center: userLocation, zoom: 20 });
 
-        const pin = new window.Microsoft.Maps.Pushpin(location,{
-          title: "bus",
+        const userPin = new window.Microsoft.Maps.Pushpin(userLocation, {
+          title: 'Your Location',
         });
-        map.entities.push(pin);
+        map.entities.push(userPin);
       }
     };
 
     loadMap();
   }, []);
 
-  return(
-     <div id="bingMap" className='w-full h-screen'></div>
-  )
+  return (
+    <div id="bingMap" className="w-full h-screen"></div>
+  );
 };
 
 export default MapComponent;

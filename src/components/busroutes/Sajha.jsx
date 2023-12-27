566df72e786c79a3a3442f2c6325d23dbe211e93
NibidaGhimire <nibidaghimire39@gmail.com>
2023-12-27 12:03:05 +0545

    bus routes

--- src/components/busroutes/Sajha.jsx
+++ src/components/busroutes/Sajha.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { SajhaRoutes } from '../../constants'
+import {  SajhaRoutes } from '../../constants'
 
 
 const SajhaR = ({ index, main, sub }) => {
@@ -11,12 +11,12 @@ const SajhaR = ({ index, main, sub }) => {
       onClick={() => setToggle(!toggle)}
       className=' red-blue-gradient p-[1px] rounded-[20px] shadow-card my-4 mx-2'>
       <button
-        className=' bg-tertiary rounded-[20px] py-4 px-8 h-fit w-fit flex justify-evenly items-center flex-col text-[20px] font-medium hover:bg-black-100'>
+        className={` ${!toggle?"bg-tertiary":"bg-black-100"} rounded-[20px] py-4 px-8 h-fit w-full flex justify-evenly items-center flex-col text-[20px] font-medium hover:bg-black-100`}>
         {main}
       </button>
 
       <div
-        className={`${!toggle ? "hidden" : "flex"} p-4 bg-black-100 absolute mx-0 my-2 min-w-[140px] z-10 rounded-xl`} >
+        className={`${!toggle ? "hidden" : "flex"} w-full justify-center flex-wrap p-4 bg-black-100 absolute mx-0 my-2 min-w-[140px] z-10 rounded-xl`} >
         <div className='flex flex-col  items-start gap-5 cursor-pointer'>
           {sub.map((title, index) => (
             <div key={index}
@@ -32,13 +32,10 @@ const SajhaR = ({ index, main, sub }) => {
 }
 
 
-
-
-
 const Sajha = () => {
   return (
     <div className='m-20 text-center h-screen'>
-      <div className='flex flex-row gap-4 flex-wrap justify-center'>
+      <div className='flex flex-col gap-4 flex-wrap justify-center'>
         {SajhaRoutes.map((route, index) => (
           <SajhaR key={route.title} index={index} {...route} />
         ))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -18,13 +18,48 @@ const SajhaRoutes = [
   {
     main: "Langankhel -- Budhanilkantha",
     sub: ["Kumaripati -- Jawalakhel -- Pulchowk","Harihar Bhavan -- Kupondole","Tripureshwor -- N.A.C -- Jamal","Lainchowr -- Lazimpat -- Panipokhari","Teaching Hospital -- Narayan Gopal Chowk","Gangalal Hospital -- Neuro Hospital -- Golfutar","Telecom Chwok -- Hattigauda -- Chapali -- Deuwa Chwok","Budhanilkantha"]
-  }
+  },
+  {
+    main: "Godawari -- Ratnapark",
+    sub: ["Godawari","Taukhel -- Hadegaun -- Badegaun","Harishidhi -- Hattiban -- Khumaltar","Satdobato -- Lagankhel -- Kumaripati -- Jawalakhel","Pulchowk -- Harihar Bhawan -- Kupondole","Tripureshwor -- N.A.C","Ratnapark"]
+  },
+  {
+    main: "Lamatar -- Ratnapark",
+    sub: ["Lamatar","Dungin -- Lubhu -- School Chwok -- Sanagaun","Kamalpokhari -- Imadole Krishna Mandir-- Ratamakai Chowk","Kist Hospital -- Gwarko -- Satdobato","Lagankhel -- Jawalakhel -- Pulchowk","Harihar Bhawan -- Kupondole -- Tripureshwor -- N.A.C","Ratnapark"]
+  },
+  {
+    main: "Thankot -- Tribhuvan International Airport",
+    sub: ["Thankot","Tribhuwan park -- Checkpost -- Satungal","Naikap -- Dua Aadha","Kalanki -- Kalanki Mandir -- Rabibhawan","Solteemode -- Kalimati -- Teku","Vipareshwor -- N.A.C -- Ratnapark","Singhadurbar -- Maitighar -- Bijali Bajar","Naya Baneshwor -- Tinkune -- Sinamangal","Tribhuvan International Airport"]
+  },
+  {
+    main: "Thankot -- Budhanilkantha",
+    sub: ["Thankot","Tribhuvan Park -- Checkpost -- Satungal","Naikap -- Kalanki -- Bafal","Sitapaila -- Swoyambhu -- Banasthali","Balaju -- Gongabu Bus Park -- Samakhusi","Basundhara- - Narayangopal Chowk -- Gangalal Hospital","Neuro Hospital -- Golfutaar","Hattigauda -- Deuba Chwok","Budhanilkantha"]
+  },
+]
+
+const NepalYatayatRoutes = [
+
+  {
+    main:"Patandhoka",
+    sub:["Patandhoka", "Kupondole -- Thapathali -- Bhadrakali","Yanyeshi -- Jamal -- Kamaladi","Kamal Pokhari -- Gyaneshwor","Ratopul -- Gaushala -- Jayabageshwori","Mitra Park --Chabahil -- Chuchepati","Tusal -- Boudha","Jorpati -- Narayantar","Dakshindhoka"]
+  },
+  {
+    main: "Lele  -- Jamal",
+    sub: ["Lele ","Tikabhairab -- Takhel -- Pyang Gaun","Thecho -- Sunakothi -- Dholahiti","Khumaltar -- Chapagaun -- Lagankhel","Kumaripati -- Jawalakhel -- Pulchwok","Hariharbhawan -- Kupondole","Tripureshwor --   N.A.C","Jamal"]
+  },
+  {
+    main: "Godawari -- Ratnapark",
+    sub: ["Godawari","Taukhel -- Hadegaun -- Badegaun","Harishidhi -- Hattiban -- Khumaltar","Satdobato -- Lagankhel -- Kumaripati -- Jawalakhel","Pulchowk -- Harihar Bhawan -- Kupondole","Tripureshwor -- N.A.C","Ratnapark"]
+  },
+  {
+    main: "Thankot -- Budhanilkantha",
+    sub: ["Thankot","Tribhuvan Park -- Checkpost -- Satungal","Naikap -- Kalanki -- Bafal","Sitapaila -- Swoyambhu -- Banasthali","Balaju -- Gongabu Bus Park -- Samakhusi","Basundhara- - Narayangopal Chowk -- Gangalal Hospital","Neuro Hospital -- Golfutaar","Hattigauda -- Deuba Chwok","Budhanilkantha"]
+  },
 ]
 
 
 
 
-
-export { navLinks, SajhaRoutes }
+export { navLinks, SajhaRoutes ,NepalYatayatRoutes}
 
 

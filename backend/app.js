6638ed1326a27422206e13258a23cf369b373f50
NibidaGhimire <nibidaghimire39@gmail.com>
2023-12-27 18:37:43 +0545

    myloc

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const faker = require('faker/locale/en'); 
+// const express = require('express');
+// const faker = require('faker/locale/en');
+// const cors = require('cors'); 
+// const app = express();
 
+// app.use(cors());
+
+// app.get('/fake-bus-location', (req, res) => {
+//     const latitude = parseFloat(faker.address.latitude());
+//     const longitude = parseFloat(faker.address.longitude());
+    
+//     res.json({ latitude, longitude });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+const express = require('express');
+const cors = require('cors'); 
 const app = express();
 
-app.get('/fake-bus-location', (req, res) => {
-    const latitude = faker.address.latitude();
-    const longitude = faker.address.longitude();
-    
-    res.json({ latitude, longitude });
+app.use(cors());
+
+app.post('/send-location', (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log('Received location:', { latitude, longitude });
+    // Process the received location data as needed
+    res.status(200).send('Location received successfully');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,8 @@
-/* eslint-disable react/no-unknown-property */
 import React from 'react'
 import { motion } from 'framer-motion'
-
 import { styles } from '../styles'
 import {fadeIn, textVariant} from '../utils/motion'
 import {SectionWrapper} from '../hoc'
-
-
-
 
 
 const About = () => {
@@ -25,7 +20,6 @@ const About = () => {
         className='mt-4 text-sec text-[17px]  leading-[30px]'
       >
       A real-time solution designed to streamline transportation. Our platform provides live bus tracking, enabling passengers to locate buses instantly, view accurate arrival times, and plan journeys efficiently. Additionally, it offers detailed bus routes and identifies the closest bus stops, enhancing commuter convenience and ensuring a seamless travel experience for all.      </motion.p>
-
     </>
   )
 }

d306b3a143c6cd1cf45a146c3593eaaaf0e38583
samriddhi004 <karkisamriddhi04@gmail.com>
2023-12-27 18:46:01 +0545

    Changed some stuff in backend

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const faker = require('faker/locale/en'); 
+// const express = require('express');
+// const faker = require('faker/locale/en');
+
+// const app = express();
+
+// app.get('/fake-bus-location', (req, res) => {
+//     const latitude = faker.address.latitude();
+//     const longitude = faker.address.longitude();
+
+//     res.json({ latitude, longitude });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+const express = require("express");
+const faker = require("faker");
 
 const app = express();
+const port = 3001;
 
-app.get('/fake-bus-location', (req, res) => {
-    const latitude = faker.address.latitude();
-    const longitude = faker.address.longitude();
-    
-    res.json({ latitude, longitude });
+app.get("/fake-bus-location", (req, res) => {
+  const fakeBusLocation = {
+    latitude: faker.address.latitude(),
+    longitude: faker.address.longitude(),
+  };
+  res.json(fakeBusLocation);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Fake GPS server is running on http://localhost:${port}`);
 });
-
-

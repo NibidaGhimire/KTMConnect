a3cc3189268e47a49f8a0cd0adc46acd131c42a7
samriddhi004 <karkisamriddhi04@gmail.com>
2023-12-27 18:36:18 +0545

    changed some url

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,33 +1,36 @@
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Track = () => {
   const [map, setMap] = useState(null);
 
   useEffect(() => {
-
     const fetchFakeBusLocation = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/fake-bus-location');
+        const response = await axios.get(
+          "http://localhost:3000/fake-bus-location"
+        );
         if (response && response.data) {
           const { latitude, longitude } = response.data;
           updateMap(parseFloat(latitude), parseFloat(longitude));
         } else {
-          console.error('Invalid response from server:', response);
+          console.error("Invalid response from server:", response);
         }
       } catch (error) {
-        console.error('Error fetching fake bus location:', error.response ? error.response.data : error.message);
+        console.error(
+          "Error fetching fake bus location:",
+          error.response ? error.response.data : error.message
+        );
       }
     };
-    
+
     const updateMap = (latitude, longitude) => {
       if (!map) return;
 
       const newLocation = new Microsoft.Maps.Location(latitude, longitude);
 
       const pin = new Microsoft.Maps.Pushpin(newLocation, {
-        title: 'Bus Location',
+        title: "Bus Location",
       });
 
       if (map.entities.getLength() > 0) {
@@ -47,8 +50,9 @@ const Track = () => {
 
   useEffect(() => {
     if (!map) {
-      const bingMapScript = document.createElement('script');
-      bingMapScript.src = 'https://www.bing.com/api/maps/mapcontrol?key=ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk&callback=initMap';
+      const bingMapScript = document.createElement("script");
+      bingMapScript.src =
+        "https://www.bing.com/api/maps/mapcontrol?jsonp=initMap&key=ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk&callback=initMap";
       window.document.body.appendChild(bingMapScript);
       window.initMap = () => {
         const newMap = new Microsoft.Maps.Map(mapRef.current, {
@@ -60,12 +64,9 @@ const Track = () => {
     }
   }, [map]);
 
-  return (
-    <div ref={mapRef} className='w-full h-screen' />
-  )
+  return <div ref={mapRef} className="w-full h-screen" />;
 };
 
 export default Track;
-
 
 // ApL9gk0lzPyXzgDct5B1kxrlkJIpkGBvFRaDYE2gVL3LNehOicd0_367zCh8WZZk
